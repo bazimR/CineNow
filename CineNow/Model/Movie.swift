d8e1b431bc052c781e01323e03e9f9a624f0469f
bazimR <rishaalmuhammed@gmail.com>
2025-04-24 02:29:23 +0530

    ApI feth fun done

---
+++ CineNow/Model/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  CineNow
+//
+//  Created by Rishal Bazim on 24/04/25.
+//
+
+import Foundation
---
+++ CineNow/Resources/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  CineNow
+//
+//  Created by Rishal Bazim on 24/04/25.
+//
+
+import Foundation
