12d17b4eb80690f5069484db1976d3212b46f17b
bazimR <rishaalmuhammed@gmail.com>
2025-05-16 00:47:54 +0530

    added few ui improvements

--- CineNow/View/HeroView.swift
+++ CineNow/View/HeroView.swift
@@ -32,42 +32,116 @@ struct HeroView: View {
                 ForEach(0..<movies.count, id: \.self) { Index in
                     let movie = movies[Index]
                     let url = URL(
-                        string: "\(Constants.API.imageBaseUrl)/w500\(movie.poster_path)"
+                        string:
+                            "\(Constants.API.imageBaseUrl)/original\(movie.poster_path)"
                     )
                     VStack {
-                        AsyncImage(url: url) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                                    .frame(width: 600, height: 600)
-                                    .background(Color.gray.opacity(0.1))
-                                    .clipShape(
-                                        RoundedRectangle(cornerRadius: 20)
-                                    )
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(
+                        ZStack {
+                            AsyncImage(url: url) { phase in
+                                switch phase {
+                                case .empty:
+                                    ProgressView().progressViewStyle(.circular)
+                                        .frame(
+                                            width: UIScreen.main.bounds.width,
+                                            height: 600
+                                        )
+                                        .scaledToFit()
+                                        .background(Color.gray.opacity(0.2))
+
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(
+                                            width: UIScreen.main.bounds.width,
+                                            height: 600
+                                        ).clipped()
+                                case .failure(_):
+                                    // When there's an error
+                                    VStack {
+                                        Image(
+                                            systemName:
+                                                "exclamationmark.triangle"
+                                        )
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(
+                                            width: 200,
+                                            height: 200
+                                        )
+                                        .foregroundColor(.red)
+
+                                    }.frame(
                                         width: UIScreen.main.bounds.width,
                                         height: 600
-                                    ).clipped()
-                            case .failure(_):
-                                // When there's an error
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .foregroundColor(.gray)
-                                    .background(Color.red.opacity(0.2))
-                                    .cornerRadius(12)
-
-                            @unknown default:
-                                VStack {
-                                    Text("Unknown error")
+                                    ).background(Color.red.opacity(0.3))
+                                        .cornerRadius(12)
+                                @unknown default:
+                                    VStack {
+                                        Text("Unknown error")
+                                    }
                                 }
                             }
-                        }
+                            VStack {
+                                Spacer()
+                                VStack(alignment: .center, spacing: 15) {
+                                    Text(movie.title)
+                                        .font(.largeTitle.bold())
+                                        .foregroundStyle(.primary)
+                                        .padding(.horizontal, 15)
+                                        .padding(.top, 5)
+                                        .multilineTextAlignment(.center)
+                                    Text(movie.overview)
+                                        .font(.caption)
+                                        .foregroundStyle(.primary.opacity(0.6))
+                                        .padding(.horizontal, 15)
+                                        .lineLimit(3)
+                                        .truncationMode(.tail)
+                                        .multilineTextAlignment(.center)
+                                    HStack {
+                                        Button(
+                                            "More info",
+                                            systemImage: "info.circle"
+                                        ) {
+
+                                        }.foregroundStyle(.black)
+                                            .buttonStyle(
+                                                .borderedProminent
+                                            )
+                                            .tint(.white)
+                                            .font(.headline)
+                                            .buttonBorderShape(
+                                                .capsule
+                                            )
+                                        Button {
+                                            // Action here
+                                        } label: {
+                                            Image(systemName: "plus")
+                                                .fontWeight(.bold)
+                                        }
+                                        .foregroundStyle(.black)
+                                        .buttonStyle(.borderedProminent)
+                                        .tint(.white.opacity(0.2))
+                                        .buttonBorderShape(.circle)
+                                        .controlSize(.large)
+                                    }
+                                }
+                                .padding(5)
+                                .padding(.vertical, 15)
+                                .background(
+                                    Material.ultraThin
+                                )
+                                .clipShape(
+                                    .rect(cornerRadius: 25)
+                                )
+                                .shadow(radius: 10)
+                                .padding()
+
+                            }
+                        }.frame(
+                            width: UIScreen.main.bounds.width,
+                            height: 600
+                        )
                     }.containerRelativeFrame(
                         .horizontal
                     )
@@ -75,7 +149,8 @@ struct HeroView: View {
             }.onAppear {
                 Task {
                     do {
-                        movies = try await TMDBService.shared.fetchPopularMovies()
+                        movies = try await TMDBService.shared
+                            .fetchPopularMovies()
                         print("success")
                         print(movies[0].poster_path)
                     } catch {
